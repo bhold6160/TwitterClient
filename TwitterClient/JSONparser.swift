c4cbdae4172e389cf547893c77e52829a41eb988
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-07-08 21:12:12 -0700

    table populating with JSON data

---
+++ TwitterClient/JSONparser.swift
@@ -0,0 +1,43 @@
+//
+//  JSONparser.swift
+//  TwitterClient
+//
+//  Created by Brandon Holderman on 7/8/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONParserCallBack = (Bool, [Tweet]?) -> ()
+
+class JSONParser {
+    static var sampleTweetData : Data {
+    
+        guard let path = Bundle.main.url(forResource: "tweet", withExtension: "json") else
+        { fatalError("No path") }
+            do {
+                let data = try Data(contentsOf: path)
+                return data
+            } catch {
+                fatalError(error.localizedDescription)
+            }
+        }
+    
+    class func tweetsFrom(data: Data, callback: JSONParserCallBack) {
+        do {
+            if let rootObject = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [[String: Any]] {
+         
+                var allTweets = [Tweet]()
+            
+                for tweetJSON in rootObject {
+                    guard let newTweet = Tweet(json: tweetJSON) else { continue }
+                    allTweets.append(newTweet)
+                }
+                
+                callback(true, allTweets)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
---
+++ TwitterClient/Tweet.swift
@@ -0,0 +1,33 @@
+//
+//  Tweet.swift
+//  TwitterClient
+//
+//  Created by Brandon Holderman on 7/8/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    let text: String
+    let id: String
+    
+    var user: User?
+    
+    init?(json: [String: Any]) {
+        if let text = json["text"] as? String,
+            let id = json["id_str"] as? String,
+        let userJSON = json["user"] as? [String: Any] {
+            
+            self.text = text
+            self.id = id
+            
+            let newUser = User(json: userJSON)
+            
+            self.user = newUser
+            
+        } else {
+            return nil
+        }
+    }
+}
