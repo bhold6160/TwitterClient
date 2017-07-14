3b2af32c65347c7d5e2786d821af7805673f9a5a
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-07-14 01:11:39 -0700

    added social and drawing from twitter API

--- TwitterClient/API.swift
+++ TwitterClient/API.swift
@@ -8,8 +8,10 @@
 
 import Foundation
 import Accounts
+import Social
 
 typealias AccountsCompletion = (ACAccount?) -> ()
+typealias TweetsCompletion = ([Tweet]?) -> ()
 
 class API {
     
@@ -24,7 +26,7 @@ class API {
         
         let accountType = accountStore.accountType(withAccountTypeIdentifier: ACAccountTypeIdentifierTwitter)
         
-        accountStore.requestAccessToAccounts(with: accountType) { (success, error) in
+        accountStore.requestAccessToAccounts(with: accountType, options: nil) { (success, error) in
             if error != nil {
                 print("ERROR: \(error!)")
                 completion(nil)
@@ -41,6 +43,67 @@ class API {
             
             print("No error, but success is FALSE")
             completion(nil)
+        }
+    }
+    
+    private func updateTimeline(completion: @escaping TweetsCompletion) {
+        
+        let url = URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+        
+        let request = SLRequest(forServiceType: SLServiceTypeTwitter,
+                                requestMethod: .GET,
+                                url: url,
+                                parameters: nil)
+        
+        print("The Service type constant for Twitter: \(SLServiceTypeTwitter)")
+        
+        request?.account = self.account
+        
+        request?.perform(handler:{ (data, response, error) in
+            if error != nil {
+                print("Error fecthing HomeTimeline: \(error!.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let response = response else {
+                completion(nil)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            switch response.statusCode {
+            case 200...299:
+                print("Success!")
+                JSONParser.tweetsFrom(data: data, callback: { (success, tweets) in
+                    completion(tweets)
+                })
+            case 400...499:
+                print("Client-side error, this is our fault... \(response.statusCode)")
+            case 500...599:
+                print("Server-side error, this is NOT our fault! \(response.statusCode)")
+            default:
+                print("Unrecognized Status Code: \(response.statusCode)")
+            }
+        })
+    }
+    
+    func getTweets(completion: @escaping TweetsCompletion) {
+        if self.account == nil {
+            self.login(completion: { (account) in
+                self.account = account
+                self.updateTimeline(completion: { (tweets) in
+                    completion(tweets)
+                })
+            })
+        } else {
+            self.updateTimeline(completion: { (tweets) in
+                completion(tweets)
+            })
         }
     }
 }
---
+++ TwitterClient/UIAcitivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIAcitivityViewController.swift
+//  TwitterClient
+//
+//  Created by Brandon Holderman on 7/12/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
+//
+
+import UIKit
+import Social
+
+//class ActivityViewController: UIActivityViewController  {
+//    
+//    @IBOutlet weak var noteTextview: UITextView!
+//    
+//    @IBAction func showShareOptions(sender: AnyObject) {
+//    }
+//    
+//    func configureNoteTextView() {
+//        noteTextview.layer.cornerRadius = 8.0
+//        noteTextview.layer.borderColor = UIColor(white: 0.75, alpha: 0.5).cgColor
+//        noteTextview.layer.borderWidth = 1.2
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        configureNoteTextView()
+//    }
+//}
