535ec889fb9d557fae8be232e184bcb6136baaf0
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-07-22 18:51:35 -0700

    resized image views

---
+++ TwitterClient/UserTimelineViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserTimelineViewController.swift
+//  TwitterClient
+//
+//  Created by Brandon Holderman on 7/22/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
+//
+
+import UIKit
+
+class UserTimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
