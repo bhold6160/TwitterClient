5aee6539ed1402326073e705caf97184f7c13ea2
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-07-24 23:58:33 -0700

    building userTimeline but cant get correct path to user info

--- TwitterClient/UserTimelineViewController.swift
+++ TwitterClient/UserTimelineViewController.swift
@@ -8,10 +8,54 @@
 
 import UIKit
 
-class UserTimelineViewController: UIViewController {
+class UserTimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var userTimelineView: UITableView!
+    
+    
+    var userTimeline: Tweet!
+    
+    var allTweets = [Tweet]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//        let nib = UINib(nibName: "TweetCell", bundle: nil)
+//        self.tableView.register(nib, forCellReuseIdentifier: "tweetCell")
+    
+        API.shared.getTweetsFor(username: userTimeline.user?.screenName, completion: { (userTweets) in
+            if let userTweets = userTweets {
+                OperationQueue.main.addOperation {
+                    self.allTweets = userTweets
+                }
+            }
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTweets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCell
+        
+        let currentTweet = self.allTweets[indexPath.row]
+        
+        cell.tweet = currentTweet
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if segue.identifier == "userViewSegue" {
+            if let selectedIndex = self.tableView.indexPathForSelectedRow {
+                let selectedTweet = self.allTweets[selectedIndex.row]
+                
+                if let destinationController = segue.destination as? UserTimelineViewController {
+                    destinationController.selectedTweet = selectedTweet
+                }
+            }
+        }
     }
 }
